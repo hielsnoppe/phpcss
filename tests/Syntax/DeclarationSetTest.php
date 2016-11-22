f4771ee212e621182ac73a911fff492680e9904d
Niels Hoppe <mail@nielshoppe.de>
2016-11-22 09:53:00 +0100

    Refactor, write tests, remove useless tests

--- tests/Syntax/DeclarationSetTest.php
+++ tests/Syntax/DeclarationSetTest.php
@@ -4,28 +4,58 @@ namespace NielsHoppe\PHPCSS\Syntax;
 
 class DeclarationSetTest extends \PHPUnit_Framework_TestCase {
 
-    /**
-     * @dataProvider validInputForCreation
-     */
+    public function testConstruct () {
 
-    public function testCanBeCreatedFromValidInput ($property, $value, $important) {
+        $obj = new DeclarationSet();
 
-        $declaration = new Declaration($property, $value, $important);
-
-        $this->assertInstanceOf(Declaration::class, $declaration);
-
-        return $declaration;
+        return $obj;
     }
 
     /**
-     * @dataProvider validInputForCreation
+     * @depends testConstruct
      */
+    public function testAddDeclaration ($obj) {
 
-    public function testCanBeSerialized ($property, $value, $important, $expected) {
+        $obj->addDeclaration(new Declaration('margin-top', '10px', false));
+        $obj->addDeclaration(new Declaration('margin-top', '20px', true));
 
-        $declaration = new Declaration($property, $value, $important);
+        return $obj;
+    }
 
-        $this->assertEquals($expected, strval($declaration));
+    /**
+     * @depends testAddDeclaration
+     */
+    public function testCreateDeclaration ($obj) {
+
+        $obj->createDeclaration('margin-bottom', '15px', false);
+
+        return $obj;
+    }
+
+    /**
+     * @depends testCreateDeclaration
+     */
+    public function testGetDeclarations ($set) {
+
+        $declarations = $set->getDeclarations();
+
+        $this->assertInternalType('array', $declarations);
+        $this->assertCount(2, $declarations);
+
+        $declarations = $set->getDeclarations(array('margin-bottom'));
+
+        $this->assertInternalType('array', $declarations);
+        $this->assertCount(1, $declarations);
+    }
+
+    /**
+     * @depends testCreateDeclaration
+     */
+    public function testToString ($obj) {
+
+        $this->assertEquals('margin-bottom: 15px; margin-top: 20px !important', strval($obj));
+
+        return $obj;
     }
 
     public function testUnion () {
@@ -45,37 +75,5 @@ class DeclarationSetTest extends \PHPUnit_Framework_TestCase {
         $baz = $foo->union($bar);
 
         $this->assertEquals('margin-bottom: 3px; margin-left: 4px; margin-right: 2px; margin-top: 1px', strval($baz));
-    }
-
-    public function testScenario () {
-
-        $obj = new DeclarationSet();
-
-        $obj->addDeclaration(new Declaration('margin-top', '10px', false));
-        $obj->addDeclaration(new Declaration('margin-top', '20px', true));
-        $obj->createDeclaration('margin-bottom', '15px', false);
-
-        $this->assertEquals('margin-bottom: 15px; margin-top: 20px !important', strval($obj));
-    }
-
-    public function validInputForCreation () {
-
-        return array(
-
-            array(
-                'font-size', '12px', null,
-                'font-size: 12px'
-            ),
-
-            array(
-                'font-size', '12px', false,
-                'font-size: 12px'
-            ),
-
-            array(
-                'font-size', '12px', true,
-                'font-size: 12px !important'
-            )
-        );
     }
 }
